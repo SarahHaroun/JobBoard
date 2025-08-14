5353888ab93c8c39728a35eb5035117404d6cc82
aisha <aisha11fathy@gmail.com>
2025-08-15 00:41:22 +0300

    update seeker

--- JobBoard.Domain/Mapping/SeekerProfileMapping.cs
+++ JobBoard.Domain/Mapping/SeekerProfileMapping.cs
@@ -9,49 +9,6 @@ using System.Threading.Tasks;
 
 namespace JobBoard.Domain.Mapping
 {
-    //public class SeekerProfileMapping : Profile
-    //{
-    //    public SeekerProfileMapping()
-    //    { // -----------------------------
-    //        // Seeker Profile
-    //        // -----------------------------
-    //        CreateMap<SeekerProfile, SeekerProfileDto>()
-    //            .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.User.PhoneNumber))
-    //            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
-    //            .ForMember(dest => dest.SkillName, opt => opt.MapFrom(src => src.Skills.Select(s => s.SkillName)))
-    //            .ForMember(dest => dest.InterestName, opt => opt.MapFrom(src => src.seekerInterests.Select(i => i.InterestName)))
-    //            .ForMember(dest => dest.CertificateName, opt => opt.MapFrom(src => src.seekerCertificates.Select(c => c.CertificateName)))
-    //            .ForMember(dest => dest.TrainingName, opt => opt.MapFrom(src => src.SeekerTraining.Select(t => t.TrainingName)))
-    //            .ForMember(dest => dest.SeekerEducations, opt => opt.MapFrom(src => src.SeekerEducations))   
-    //            .ForMember(dest => dest.SeekerExperiences, opt => opt.MapFrom(src => src.SeekerExperiences));
-
-
-    //        //if value is null, it will not be mapped
-    //        CreateMap<SeekerProfileUpdateDto, SeekerProfile>()
-    //            .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
-
-
-    //        // -----------------------------
-    //        // Education
-    //        // -----------------------------
-    //        CreateMap<SeekerEducation, SeekerEducationDto>().ReverseMap();
-
-    //        CreateMap<SeekerEducationUpdateDto, SeekerEducation>()
-    //            .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
-
-
-    //        // -----------------------------
-    //        // Experience
-    //        // -----------------------------
-    //        CreateMap<SeekerExperience, SeekerExperienceDto>().ReverseMap();
-
-    //        CreateMap<SeekerExperienceUpdateDto, SeekerExperience>()
-    //            .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
-    //    }
-
-
-    //}
-
 
     public class SeekerProfileMapping : Profile
     {
