2ebc2e7b669a147781cb295dd769a26208e30734
ManarMahmoudAmin <manareltayeb825@gmail.com>
2025-08-25 01:12:36 +0300

    update seeding data

---
+++ JobBoard.Repositories/Data/Migrations/20250824210829_change delete behavior of job.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobBoard.Repositories.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changedeletebehaviorofjob : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_EmployerProfiles_EmployerId",
+                table: "Jobs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_EmployerProfiles_EmployerId",
+                table: "Jobs",
+                column: "EmployerId",
+                principalTable: "EmployerProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_EmployerProfiles_EmployerId",
+                table: "Jobs");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_EmployerProfiles_EmployerId",
+                table: "Jobs",
+                column: "EmployerId",
+                principalTable: "EmployerProfiles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
